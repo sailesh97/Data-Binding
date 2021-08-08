ae4b0b3480b409ba9b43d6d4ab0255cd9b617ecd
Sailesh Patra <saileshpatra997@gmail.com>
2021-08-09 00:02:15 +0530

    Introduction to ViewChild- another way of accessing the DOM like local ref and two way binding

--- src/app/cockpit/cockpit.component.ts
+++ src/app/cockpit/cockpit.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, EventEmitter, Output } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output, ViewChild, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-cockpit',
@@ -11,20 +11,47 @@ export class CockpitComponent implements OnInit {
   newServerName = '';
   newServerContent = '';
 
+  @ViewChild('serverContentInput2', {static: true}) serverContentInput: ElementRef;  
+  /**
+   * ViewChild takes two args. A string : that is the selector. How we want to select a element
+   *  a. The selector we use here is not a normal css selector but we can use the name of a local reference.
+   *  b. If you want to select a custom component made by us, you can mention the class name of the custom component without any double quotes instead of a string.
+   * 
+   * The 2nd arg is an object that is {static: true}.
+   * 
+   * ex:
+   *  @ViewChild(CockpitComponent)
+   *  @ViewChlid('serverNameInput') 
+   * 
+   * ViewChild will give us the 1st occurrence of the element we want to get. 
+   *  Like if there are 2 cockpit components used in our dom, it will return the 1st one always
+   * 
+   * serverContentInput2 in DOM can be accessed by serverContentInput
+   * 
+   * ViewChild will return an output of type ElementRef.
+   * 
+   * ElementRef is an angular type. 
+   * 
+   * The output of type ElementRef has a property called nativeElement which indeed holds the actual HTML element.
+   * 
+   * So we can get the value property from the nativeElement of HTML Element. 
+   */
   constructor() { }
 
   ngOnInit(): void {
   }
 
   onAddServer(nameInput: HTMLInputElement) {
-    console.log(nameInput)
-    this.serverCreated.emit({serverName: nameInput.value, serverContent: this.newServerContent});
+    console.log("Through Local Ref",nameInput);
+    console.log("Through ViewChild", this.serverContentInput);
+    this.serverCreated.emit({serverName: nameInput.value, serverContent: this.serverContentInput.nativeElement.value});
+    // this.serverCreated.emit({serverName: nameInput.value, serverContent: this.newServerContent});
     // this.serverCreated.emit({serverName: this.newServerName, serverContent: this.newServerContent});
   }
 
   onAddBlueprint(nameInput: HTMLInputElement) {
-    this.blueprintCreated.emit({serverName: nameInput.value, serverContent: this.newServerContent});
-
+    this.blueprintCreated.emit({serverName: nameInput.value, serverContent: this.serverContentInput.nativeElement.value});
+    // this.blueprintCreated.emit({serverName: nameInput.value, serverContent: this.newServerContent});
   }
 
   /**
@@ -34,5 +61,13 @@ export class CockpitComponent implements OnInit {
    * 
    * To make this event accesible/listenable to trigger from outside this component, we need to mention Output decorator.
    * 
+   */
+
+  /**
+   * this.serverContentInput.nativeElement.value = 'somethong' is not recommended at all.
+   * 
+   * Accessing the DOM this way is not recommended at all. We can show things in UI through property binding/string interpolation etc.
+   * 
+   * There are better ways of accessing the DOM in Angular that we will learn in directives section.
    */
 }
